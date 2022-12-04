787fe00a5e54c5f2a41610be7a42047f84b8adad
MisterrHappy <andremarinho_1@hotmail.com>
2022-12-04 18:10:06 +0000

    day4 - 2022

---
+++ 2022/day4/solutionPart1.py
@@ -0,0 +1,24 @@
+total_overlaps = 0
+
+def check_overlap(left_start: int, right_start: int, left_end: int, right_end: int) -> bool:
+    return left_start >= right_start and left_end <= right_end
+
+with open("input.txt", "r") as input_file:
+    lines = input_file.readlines()
+    for line in lines:
+        left_elf_range, right_elf_range = line.split(",")
+        left_elf_start, left_elf_end = left_elf_range.split("-") 
+        right_elf_start, right_elf_end = right_elf_range.split("-") 
+
+        left_start = int(left_elf_start)
+        left_end = int(left_elf_end.strip())
+
+        right_start = int(right_elf_start)
+        right_end = int(right_elf_end.strip())
+
+        if check_overlap(left_start, right_start, left_end, right_end) or \
+            check_overlap(right_start, left_start, right_end, left_end):
+            total_overlaps += 1
+
+
+print(f"Number of total overlaps is: {total_overlaps}")
