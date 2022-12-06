e7ccf75eda915c1e0b4ac4528e8cf39be1fc8721
MisterrHappy <andremarinho_1@hotmail.com>
2022-12-06 23:21:09 +0000

    day 6 - 2022

---
+++ 2022/day6/solutionPart2.py
@@ -0,0 +1,17 @@
+number_of_chars = 0
+
+with open("input.txt", "r") as input_file:
+    stream = input_file.readlines()[0].strip()
+    marker = ''
+    for c in stream:
+        if len(marker) == 14:
+            break
+
+        number_of_chars += 1
+        index_of_char = marker.find(c)
+        if index_of_char != -1:
+            marker = marker[index_of_char + 1:] + c
+        else:
+            marker += c
+            
+print(f"The number of processed chars before marker is: {number_of_chars}")
